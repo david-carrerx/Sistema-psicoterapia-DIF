3d2635fe7a3a2b853e7b32fc70d3903d03b93bb2
david-carrerx <lerma.angel.isw@gmail.com>
2023-10-20 14:21:50 -0600

    Login de usuarios en front

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,29 +1,29 @@
-@extends('layouts.app')
+@extends('layouts.app-login')
 
 @section('title', 'Iniciar sesión')
 @section('content')
-    <h1>Iniciar sesión</h1>
-
-     <!--Formulario de login de usuario-->
-     <form method="POST" action="{{ route('login') }}">
-        @csrf
-        <label for="">
-            <input name="email" type="email" required autofocus value="{{ old('email') }}" placeholder="Email" autocomplete="off">
-        </label>
-        @error('email') {{$message}} @enderror
-        <br>
-        <label for="">
-            <input name="password" type="password" placeholder="Contraseña">
-        </label>
-        @error('password') {{$message}} @enderror
-        <br>
-        <label for="">
-            <input type="checkbox" name="remember" id="">
-            Recordar sesión
-        </label><br>
-        <label>
-            <span>¿No tienes cuenta? <a href="register">registrate aquí</a></span>
-        </label>
-        <button type="submit">Acceder</button>
-    </form>
+    <!--Imagen del borde superior-->
+    <div class="header-image"></div>    
+        <main>
+            <!--Formulario de login de usuario-->
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <img src="https://www.difdurango.gob.mx/wp-content/uploads/2022/12/dif.svg" class="card-img-top" alt="..." style="margin-bottom: 15px;">
+                <label for="email"><span face="Inter" size=5>Correo de usuario</span></label>
+                <input id="email" name="email" type="email" required autofocus value="{{ old('email') }}" placeholder="Email" autocomplete="off">
+                @error('email') <div style="color:red">{{$message}}</div>@enderror
+        
+                <label for="password"><span face="Inter" size=5>Contraseña</span></label>
+                <input id="password" name="password" type="password" placeholder="Contraseña">
+                @error('email') <div style="color:red">{{$message}} @enderror
+                <br><br>
+                <!--<label for=""><span>Recordar sesión</span><input type="checkbox" name="remember" id=""></label>-->
+                <!--<label>
+                <span>¿No tienes cuenta? <a href="register">registrate aquí</a></span>
+                </label>-->
+                <button type="submit">Acceder</button>
+            </form>
+        </main>
+    <!--Imagen del borde inferior-->
+    <div class="footer-image"></div>
 @endsection
--- resources/views/layouts/app-login.blade.php
+++ resources/views/layouts/app-login.blade.php
@@ -1,4 +1,4 @@
-<!--Estructura general de las vistas-->
+<!--Estructura general de la vista login-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,12 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Psicoterapia - @yield('title') </title>
-    <script src="/resources/js/register.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/login-styles.css') }}" rel="stylesheet">
 </head>
 <body>
     @include('partials.nav')
     @yield('content')
-    <!--Si se encuentra un mensaje de status este se imprime-->
+    <!--Si se encuentra un mensaje de status este se muestra como alerta-->
     @if(session('status'))
         <script>
             document.addEventListener("DOMContentLoaded", function()
