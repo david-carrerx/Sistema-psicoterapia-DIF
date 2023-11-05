05816f64539c4daf0342127d489cff029b0b78c6
david-carrerx <lerma.angel.isw@gmail.com>
2023-11-04 19:13:17 -0600

    Estilos del filtro de pacientes completado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <title>Psicoterapia - @yield('title') </title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{ asset('css/sidemenu.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/patients.css')}}" rel="stylesheet">
     <link rel="icon" href="{{asset('assets/icon.png')}}">
 </head>
 <body>
--- resources/views/patients.blade.php
+++ resources/views/patients.blade.php
@@ -4,34 +4,53 @@
 @section('content')
 
     <!--Filtro de búsqueda de pacientes-->
-    <form method="POST" action="{{ route('buscar-pacientes') }}">
-        @csrf
-        <label for="id">Buscar por ID</label>
-        <input type="text" name="id" placeholder="ID del paciente" autocomplete="off">
-        <label for="name">Buscar por nombre</label>
-        <input type="text" name="name" placeholder="Nombre del paciente" autocomplete="off">
-        <label for="psychologist">Buscar por psicólogo</label>
-          <select name="psychologist" id="psychologist">
-            <option value="" disabled selected>Seleccionar</option>
-                @foreach ($psychologists as $psychologist)
-                    <option value="{{ $psychologist->id }}">{{ $psychologist->name }}</option>
-                @endforeach
-            </select>
-        <button>Buscar</button>
-
-         <!--Botón para ver de nuevo a todos los pacientes-->
-        <form method="GET" action="{{ route('pacientes') }}">
+    <div class="container">
+        <form method="POST" action="{{ route('buscar-pacientes') }}">
             @csrf
-            <button type="submit">Ver todos</button>
-        </form>
-    </form>
-    <a href="{{ route('agregar-pacientes') }}">Agregar</a>
+            <div class="row">
+                <div class="col order-last"></div>
+                <div class="col">
+                    <label for="psychologist">Buscar por nombre</label>
+                </div>
+                <div class="col psy">
+                    <label for="name">Buscar por psicólogo</label>
+                </div>
+                <div class="col order-first">
+                    <label for="id">Buscar por ID</label>
+                </div>
+            </div>
 
+            <div class="row">
+                <div class="col order-last">
+                    <!--Botón para ver de nuevo a todos los pacientes-->
+                    <a href="{{ route('pacientes') }}" type="submit" class="custom-button">Ver todos<img src="{{ asset('assets/filter.svg') }}" alt="..." class="button-icon"> </a>
+                    <a href="{{ route('agregar-pacientes') }}" type="submit" class="custom-button2">Agregar<img src="{{ asset('assets/add.svg')}}" alt="..." class="button-icon"></a>
+                </div>
+
+                <div class="col">
+                    <input type="text" name="name" placeholder="Nombre del paciente" autocomplete="off">
+                </div>
+                <div class="col psy">
+                    <select name="psychologist" id="psychologist">
+                        <option value="" disabled selected>Seleccionar</option>
+                            @foreach ($psychologists as $psychologist)
+                                <option value="{{ $psychologist->id }}">{{ $psychologist->name }}</option>
+                            @endforeach
+                        </select>
+                    <button type="submit" class="btn btn-success">Buscar</button>
+                </div>
+                <div class="col order-first">
+                    <input type="text" name="id" placeholder=" ID del paciente" autocomplete="off">
+                </div>
+            </div>                            
+        </form>
+    </div>
+    <br>
     <!--Tabla con los datos de pacientes-->
-    <table>
+    <table class="table table-striped">
         <thead>
             <tr>
-                <th>ID</th>
+                <th class="id">ID</th>
                 <th>Nombre</th>
                 <th>Teléfono</th>
                 <th>Psicólogo asignado</th>
@@ -42,12 +61,12 @@
         <tbody>
             @foreach ($patients as $patient)
                 <tr>
-                    <td>{{ $patient->id }}</td>
+                    <td class="id">{{ $patient->id }}</td>
                     <td>{{ $patient->name }}</td>
                     <td>{{ $patient->phone }}</td>
                     <td>{{ $patient->psychologist->name }}</td>
-                    <td>{{ $patient->status }}</td>
-                    <td><a href="{{ route('ver-expediente' , ['id' => $patient->id]) }}">Expediente</a></td>
+                    <td><div class="estado-container activo">{{ $patient->status }}</div></td>
+                    <td><a type="button" href="{{ route('ver-expediente' , ['id' => $patient->id]) }}" class="expediente-button">Expediente<img src="{{asset('assets/eye.png')}}" alt="..." class="button-icono"></a></td>
                 </tr>
             @endforeach
         </tbody>
